8588347d922b2316fee18bd15f7d5eee20e29756
Matteo Nastasi <nastasi@openquake.org>
2017-01-26 14:32:10 +0100

    more stuff to manage wheels

---
+++ debian/makedeb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GEM_GIT_PACKAGE="oq-libs"
+GEM_DEB_PACKAGE="python-${GEM_GIT_PACKAGE}"
+rm -rf build-deb
+mkdir build-deb
+git archive --prefix ${GEM_GIT_PACKAGE}/ HEAD | (cd build-deb ; tar xv)
+cd build-deb/${GEM_GIT_PACKAGE}
+debuild -S  -us -uc -i
