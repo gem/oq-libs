ac780ee08fbcbdcf90f94156493a54ff421547a8
Matteo Nastasi <nastasi@openquake.org>
2017-01-26 16:30:29 +0100

    create sources instead of deb binaries

--- helpers/makedeb.sh
+++ helpers/makedeb.sh
@@ -5,5 +5,5 @@ rm -rf build-deb
 mkdir build-deb
 git archive --prefix ${GEM_GIT_PACKAGE}/ HEAD | (cd build-deb ; tar xv)
 cd build-deb/${GEM_GIT_PACKAGE}
-# debuild -S  -us -uc -i
-debuild -b -us -uc -i
+debuild -S -i
+# debuild -b -us -uc -i
