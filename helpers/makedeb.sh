9ae4165d6e7dbb5f2812c8913ee658ff50d61bfe
Matteo Nastasi <nastasi@openquake.org>
2017-02-14 13:44:54 +0100

    wrong debian/changelog version management fixed [2]

--- helpers/makedeb.sh
+++ helpers/makedeb.sh
@@ -6,7 +6,7 @@ GEM_DEB_PACKAGE="python-${GEM_GIT_PACKAGE}"
 
 if [ "$1" = "check_versions" ]; then
     vers_python="$(python -c "from openquake.libs import __version__ ; print __version__")"
-    vers_debian="$(head -n 1 debian/changelog  | sed 's/^.*(//g;s/).*//g;s/-.*//g')"
+    vers_debian="$(head -n 1 debian/changelog  | sed 's/^.*(//g;s/).*//g;s/[-~].*//g')"
 
     if [ "$vers_python" != "$vers_debian" ]; then
         echo "Python version (openquake/libs/__init__.py) and debian version are different ($vers_python != $vers_debian)"
