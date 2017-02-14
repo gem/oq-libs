868d0f40fa13c3ef3b11428d44a823ebca1877b5
Matteo Nastasi <nastasi@openquake.org>
2017-02-14 11:10:14 +0100

    update packager.sh to manage version from libs/__init__.py file

--- packager.sh
+++ packager.sh
@@ -602,8 +602,8 @@ fi
 
 cd "$GEM_BUILD_SRC"
 
-# version info from openquake/hazardlib/__init__.py
-ini_vers="$(cat openquake/hazardlib/__init__.py | sed -n "s/^__version__[  ]*=[    ]*['\"]\([^'\"]\+\)['\"].*/\1/gp")"
+# version info from openquake/libs/__init__.py
+ini_vers="$(cat openquake/libs/__init__.py | sed -n "s/^__version__[  ]*=[    ]*['\"]\([^'\"]\+\)['\"].*/\1/gp")"
 ini_maj="$(echo "$ini_vers" | sed -n 's/^\([0-9]\+\).*/\1/gp')"
 ini_min="$(echo "$ini_vers" | sed -n 's/^[0-9]\+\.\([0-9]\+\).*/\1/gp')"
 ini_bfx="$(echo "$ini_vers" | sed -n 's/^[0-9]\+\.[0-9]\+\.\([0-9]\+\).*/\1/gp')"
