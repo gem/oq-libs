2a97ee229d8a9a2fd79258ac2095c39ec9d1f28b
Antonio Ettorre <vot4anto@gmail.com>
2022-02-14 09:00:52 +0100

    update version of scipy to 1.7.3, and oq-libs to 3.4.0

--- openquake/libs/__init__.py
+++ openquake/libs/__init__.py
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '3.3.0'
+__version__ = '3.4.0'
